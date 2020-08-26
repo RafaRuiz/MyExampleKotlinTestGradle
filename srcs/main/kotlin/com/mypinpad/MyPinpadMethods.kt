7d8d34f36d91713d88eb1e245a3be27ea4193953
Rafael Ruiz Muñoz <raruizmu@gmail.com>
2020-08-26 16:10:21 +0200

    Main method

---
+++ srcs/main/kotlin/com/mypinpad/MyPinpadMethods.kt
@@ -0,0 +1,18 @@
+package com.mypinpad
+
+/**
+ * function isInRange takes a:
+ * @param subject which is an optional String (nullable) and
+ * @return true if the first letter is between the range [A-Z] or
+ * @return false otherwise
+ *
+ * The function is optimized so it doesn't take as long as other similar
+ * solutions (creating ranges such as ('A'..'Z'), using `.contains()`, etc)
+ */
+fun isInRange(subject: String?): Boolean {
+    return subject
+        ?.getOrNull(0)
+        ?.toString()
+        ?.matches(Regex("[A-Z]"))
+        ?: false
+}
